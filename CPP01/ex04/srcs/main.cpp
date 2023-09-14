4e5cec0b4e51c47e7592c995d5f03c0c9579f3d6
Lucas-Ve <lucas@DESKTOP-7QDI0MF.localdomain>
2023-09-14 10:26:02 +0200

    CPP01 ex04

--- CPP01/ex04/srcs/main.cpp
+++ CPP01/ex04/srcs/main.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(int argc, char *argv[])
 {
-    std::string out_file;
-    if (argc == 4)
+    std::string str;
+    std::string s1;
+    std::string s2;
+    std::size_t pos;
+
+    if (argc == 4 && argv[2][0] != '\0')
     {
-        out_file = argv[1];
-        out_file.insert(out_file.length(), ".replace");
-        std::cout << out_file << std::endl;
+        s1 = argv[2];
+        s2 = argv[3];
         std::ifstream ifs(argv[1]);
-        std::ofstream ofs(out_file);
+        std::ofstream ofs((std::string(argv[1]) + ".replace").c_str());
+        while (std::getline(ifs, str))
+        {
+            pos = 0;
+            while ((pos = str.find(s1, pos)) != std::string::npos)
+            {
+                str.erase(pos, s1.length());
+                str.insert(pos, s2);
+                pos += s2.length();
+            }
+            ofs << str;
+        }
         ifs.close();
         ofs.close();
     }
+    else if (argv[2][0] == '\0')
+        std::cout << "Second argument can't be empty." << std:: endl;
+    else
+        std::cout << "Wrong number of arguments !!" << std:: endl;
     return (0);
 }
