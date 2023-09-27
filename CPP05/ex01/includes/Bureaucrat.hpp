0c8de167e348810e4ff8c961c656f2ec8b3d8682
Lucas Veloso <lveloso@z2r1p8.42lyon.fr>
2023-09-27 11:24:41 +0200

    CPP04 ex01 finish

---
+++ CPP05/ex01/includes/Bureaucrat.hpp
@@ -0,0 +1,49 @@
+# ifndef BUREAUCRAT_H
+#define BUREAUCRAT_H
+
+#include <iostream>
+#include <stdexcept>
+#include "../includes/Form.hpp"
+class Form;
+
+class Bureaucrat
+{
+private:
+    std::string _name;
+    int _grade;
+public:
+    Bureaucrat(void);
+    Bureaucrat(std::string const name, int grade);
+    Bureaucrat(Bureaucrat const &src);
+    ~Bureaucrat();
+
+    Bureaucrat &operator=(Bureaucrat const &rhs);
+
+    std::string getName(void)const;
+    int getGrade(void)const;
+    void upGrade(void);
+    void downGrade(void);
+
+    void signForm(Form &form);
+
+    class GradeToHighException: public std::exception
+    {
+    public:
+        virtual const char* what() const throw(){
+            return ("Grade to high !!");
+        }
+        
+    };
+
+    class GradeToLowException: public std::exception
+    {
+    public:
+        virtual const char* what() const throw(){
+            return ("Grade to low !!");
+        }
+    };
+};
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &rhs);
+
+#endif
---
+++ CPP05/ex01/srcs/Bureaucrat.cpp
@@ -0,0 +1,80 @@
+#include "../includes/Bureaucrat.hpp"
+
+
+Bureaucrat::Bureaucrat(void)
+{
+    std::cout << "Constructor default Bureaucrat" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string const name, int grade):_name(name)
+{
+    std::cout << "Constructor with name, grade Bureaucrat" << std::endl;
+    if (grade < 1)
+        throw(Bureaucrat::GradeToHighException());
+    else if (grade > 150)
+        throw(Bureaucrat::GradeToLowException());
+    this->_grade = grade;
+    
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &src)
+{
+    std::cout << "Constructor copy Bureaucrat" << std::endl;
+    *this = src;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Destructor Bureaucrat" << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &rhs)
+{
+    this->_name = rhs.getName();
+    this->_grade = rhs.getGrade();
+    return (*this);
+}
+
+std::string Bureaucrat::getName(void)const
+{
+    return (this->_name);
+}
+
+int Bureaucrat::getGrade(void)const
+{
+    return (this->_grade);
+}
+
+void Bureaucrat::downGrade(void)
+{
+    if (this->_grade + 1 > 150)
+        throw(Bureaucrat::GradeToLowException());
+    this->_grade++;
+}
+
+void Bureaucrat::upGrade(void)
+{
+    if (this->_grade - 1 < 1)
+        throw(Bureaucrat::GradeToHighException());
+    this->_grade--;
+}
+
+void    Bureaucrat::signForm(Form &form)
+{
+    try
+    {
+        form.beSigned(*this);
+        std::cout << this->_name << " signed " << form.getName() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << this->_name << " couldn't sign " << form.getName() << " because " << e.what() << std::endl;
+    }
+    
+}
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &rhs)
+{
+    o << rhs.getName() << ", bureaucrat grade " << rhs.getGrade() << ".";
+    return (o);
+}
