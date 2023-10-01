f3e39169dcb7de454870ea36709d0f2c04871703
Lucas-Ve <lucas@DESKTOP-7QDI0MF.localdomain>
2023-10-01 21:06:28 +0200

    CPP07 ex02 in progress

--- CPP07/ex02/includes/Array.hpp
+++ CPP07/ex02/includes/Array.hpp
@@ -5,19 +5,41 @@ template<typename T>
 class Array
 {
 private:
-    /* data */
+    T *_array;
+	unsigned int _size;
 public:
-    Array(/* args */);
-    ~Array();
+    Array():_size(0)
+    {
+        this->_array = new T[this->_size];
+    }
+
+    Array(unsigned int n):_size(n)
+    {
+        this->_array = new T[this->_size];
+    }
+
+    Array(Array const &src): _size(src._size)
+    {
+        this->_tab = new T[_size];
+
+        for (unsigned int i = 0; i < this->_size; i++)
+            this->_tab[i] = src._tab[i];
+    }
+
+    ~Array()
+    {
+        if (this->_array != NULL)
+			delete [] this->_array;
+    }
+
+    Array &operator=(Array const &rhs): _size(rhs._size)
+    {
+        this->_tab = new T[_size];
+
+        for (unsigned int i = 0; i < this->_size; i++)
+            this->_tab[i] = rhs._tab[i];
+        return (*this);
+    }
 };
-
-Array::Array(/* args */)
-{
-}
-
-Array::~Array()
-{
-}
-
 
 #endif
