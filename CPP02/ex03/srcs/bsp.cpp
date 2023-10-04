ef98c7729c9d7e9cdcbf670c5969b7a482d490fa
Lucas Veloso <lveloso@z2r1p8.42lyon.fr>
2023-10-04 13:05:31 +0200

    correction point sur le triangle

--- CPP02/ex03/srcs/bsp.cpp
+++ CPP02/ex03/srcs/bsp.cpp
@@ -7,7 +7,7 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
     Fixed E2 = ((a.getX() - point.getX()) * (b.getY() - point.getY()) - (a.getY() - point.getY()) * (b.getX() - point.getX()));
     Fixed E3 = ((b.getX() - point.getX()) * (c.getY() - point.getY()) - (b.getY() - point.getY()) * (c.getX() - point.getX()));
     
-    if ((E1 >= 0 && E2 >= 0 && E3 >= 0) || (E1 <= 0 && E2 <= 0 && E3 <= 0))
+    if ((E1 > 0 && E2 > 0 && E3 > 0) || (E1 < 0 && E2 < 0 && E3 < 0))
         return (true);
     else
         return (false);
