7425b23184a627c6c493441ed621cd737d979ccf
Lucas Veloso <lveloso@z1r4p5.42lyon.fr>
2023-10-10 10:06:57 +0200

    main change

--- CPP03/ex01/srcs/main.cpp
+++ CPP03/ex01/srcs/main.cpp
@@ -9,26 +9,33 @@ int	main(void)
 	ClapTrap claptrap(s1);
 	ScavTrap scavtrap(s2);
 
+	std::cout << claptrap << std::endl;
+	std::cout << scavtrap << std::endl;
 	std::cout << std::endl;
 	claptrap.attack(s2);
 	scavtrap.takeDamage(0);
 	scavtrap.beRepaired(18);
 	std::cout << std::endl;
+	std::cout << claptrap << std::endl;
+	std::cout << scavtrap << std::endl;
 	scavtrap.attack(s1);
 	claptrap.takeDamage(20);
 	scavtrap.attack(s1);
 	claptrap.takeDamage(20);
-	scavtrap.attack(s1);
 	claptrap.beRepaired(64);
 	std::cout << std::endl;
+	std::cout << claptrap << std::endl;
+	std::cout << scavtrap << std::endl;
 	scavtrap.guardGate();
-	scavtrap.attack(s1);
 	std::cout << std::endl;
+	std::cout << claptrap << std::endl;
+	std::cout << scavtrap << std::endl;
 	scavtrap.takeDamage(100);
-	scavtrap.takeDamage(1);
-	scavtrap.attack(s2);
+	scavtrap.takeDamage(100);
 	scavtrap.beRepaired(200);
 	
 	std::cout << std::endl;
+	std::cout << claptrap << std::endl;
+	std::cout << scavtrap << std::endl;
 	return (0); 
 }
