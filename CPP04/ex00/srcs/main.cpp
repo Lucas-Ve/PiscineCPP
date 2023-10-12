ec5c4d9899188b0947221b83c17948ecc5d41042
Lucas Veloso <lveloso@z2r1p8.42lyon.fr>
2023-10-12 09:25:08 +0200

    main change

--- CPP04/ex00/srcs/main.cpp
+++ CPP04/ex00/srcs/main.cpp
@@ -10,14 +10,16 @@ int main()
     const Animal *j = new Dog("titi");
     const Animal *i = new Cat("chat");
     const WrongAnimal* k = new WrongCat("mouf");
+    std::cout << std::endl;
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
     std::cout << k->getType() << " " << std::endl;
+    std::cout << std::endl;
     i->makeSound();
     j->makeSound();
     meta->makeSound();
     k->makeSound();
-
+    std::cout << std::endl;
     delete k;
     delete meta;
     delete j;
