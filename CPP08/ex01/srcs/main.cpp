ba6c72ba9f9c4941f3edd62b8bb284830d7dd936
Lucas-Ve <lucasveloso@sfr.fr>
2023-10-25 21:11:56 +0200

    change main ex01 CPP08

--- CPP08/ex01/srcs/main.cpp
+++ CPP08/ex01/srcs/main.cpp
@@ -2,18 +2,18 @@
 
 int main()
 {
-    // {
-    //     std::cout << "================TEST SUBJECT================" << std::endl;
-    //     Span sp = Span(5);
-    //     sp.addNumber(6);
-    //     sp.addNumber(3);
-    //     sp.addNumber(17);
-    //     sp.addNumber(9);
-    //     sp.addNumber(11);
-    //     std::cout << sp.shortestSpan() << std::endl;
-    //     std::cout << sp.longestSpan() << std::endl;
-    //     std::cout << "============================================" << std::endl << std::endl;
-    // }
+    {
+        std::cout << "================TEST SUBJECT================" << std::endl;
+        Span sp = Span(5);
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
+        std::cout << "============================================" << std::endl << std::endl;
+    }
 
     // {
     //     try 
@@ -42,22 +42,21 @@ int main()
     //         std::cout << e.what() << std::endl; 
     //     }
     // }
-    try
-    {
-        Span span3(6);
-        span3.addNumber(6);
-        std::vector<int> range(5, 10);
-        // range[2] = 40;
-        // std::cout << span3.shortestSpan() << std::endl;
-        span3.addNumber(range.begin(), range.end());
-        std::cout << span3.longestSpan() << std::endl;
-        // for (std::vector<int>::iterator it = range.begin(); it != range.end(); it++)
-        //     std::cout << *it << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+
+    // try
+    // {
+    //     Span span3(6);
+    //     span3.addNumber(6);
+    //     std::vector<int> range(5, 10);
+    //     span3.addNumber(range.begin(), range.end());
+    //     std::cout << span3.longestSpan() << std::endl;
+    //     for (std::vector<int>::iterator it = range.begin(); it != range.end(); it++)
+    //         std::cout << *it << std::endl;
+    // }
+    // catch(const std::exception& e)
+    // {
+    //     std::cerr << e.what() << '\n';
+    // }
     
     return 0;
 }
